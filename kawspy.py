7257e0f7d74479a1ce32c3939e57987cc898be34
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-08-02 19:08:28 +0530

    test

---
+++ kawspy.py
@@ -0,0 +1,50 @@
+import boto3
+iotanalytics = boto3.client("iotanalytics")
+sns = boto3.client("sns")
+from os import environ
+ddb = boto3.client("dynamodb")
+translate = boto3.client("translate")
+
+def handler(event, context):
+    try:
+        data = translate.translate_text(
+            SourceLanguageCode="auto",
+            TargetLanguageCode="en",
+            Text="Hola"
+        )
+        print(data)
+    except BaseException as e:
+        print(e)
+        raise(e)
+        try:
+            data = ddb.scan(
+                TableName="KChineseAnimal"
+            )
+            print(data)
+        except BaseException as e:
+            print(e)
+            raise(e)
+            try:
+                data = sns.get_topic_attributes(
+                    TopicArn="arn:aws:sns:us-east-1:318300609668:Notifications"
+                )
+                print(data)
+            except BaseException as e:
+                print(e)
+                raise(e)
+                try:
+                    data = iotanalytics.batch_put_message(
+                        channelName="my_channel",
+                        messages=[
+                            {
+                                'messageId': "ID",
+                                'payload': "Test Message"
+                            }
+                        ]
+                    )
+                    print(data)
+                except BaseException as e:
+                    print(e)
+                    raise(e)
+    
+    return {"message": "Successfully executed"}
---
+++ ksubi/lamdainsub.py
@@ -0,0 +1,24 @@
+import ask_sdk_core
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+
+def handler(event, context):
+    sb = SkillBuilder()
+    
+    sb.add_exception_handler(ErrorHandler())
+    #delete undefined built-in intent handlers
+    sb.add_request_handler(CancelIntentHandler())
+    sb.add_request_handler(HelpIntentHandler())
+    sb.add_request_handler(StopIntentHandler())
+    sb.add_request_handler(NavigateHomeIntentHandler())
+    sb.add_request_handler(FallbackIntentHandler())
+    sb.add_request_handler(LaunchRequestHandler())
+    sb.add_request_handler(SessionEndedRequestHandler())
+    #add custom request handlers
+    #sb.add_request_handler(CustomIntentHandler())
+    
+    def handler(event, context):
+        return sb.lambda_handler()(event, context)
+    
+    return {"message": "Successfully executed"}
